e288019a9d2409be7d2976bba02a29ee7aa112b5
Karl Eklund <localpart@gmail.com>
2025-02-18 23:44:57 +0100

    Set font

--- init.el
+++ init.el
@@ -5,3 +5,6 @@
 (setq mac-option-modifier 'none)
 
 (load (concat user-emacs-directory "packages.el"))
+
+(set-frame-font "Hack Nerd Font Mono" nil t)
+(set-face-attribute 'default nil :font "Hack Nerd Font Mono" :height 140)
