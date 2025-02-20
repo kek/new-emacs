e3b4cf46509812f4d1478bc3f97a8c50ae897306
Karl Eklund <localpart@gmail.com>
2025-02-20 11:06:31 +0100

    Change theme

--- packages.el
+++ packages.el
@@ -25,7 +25,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-wilmersdorf t)
+  (load-theme 'doom-moonlight t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
