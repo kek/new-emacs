84965990eb052e6338cd767167597698e3312eac
Karl Eklund <localpart@gmail.com>
2025-02-24 01:27:21 +0100

    f8 find todo file

--- keys.el
+++ keys.el
@@ -1,1 +1,2 @@
 (global-set-key (kbd "<f9>") (lambda () "" (interactive) (find-file (concat user-emacs-directory "init.el"))))
+(global-set-key (kbd "<f8>") (lambda () "" (interactive) (find-file (concat user-emacs-directory "todo.org"))))
