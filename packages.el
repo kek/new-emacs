429403ad629d98f13c75f4d89ee1bec48ec88a6e
Karl Eklund <localpart@gmail.com>
2025-03-01 22:33:50 +0100

    theme

--- packages.el
+++ packages.el
@@ -26,7 +26,9 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-moonlight t)
+  (if (eq system-type 'gnu/linux)
+      (load-theme 'doom-sourcerer t)
+    (load-theme 'doom-moonlight t))
   (my/set-font)
 
   ;; Enable flashing mode-line on errors
