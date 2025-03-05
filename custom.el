74302e333b96cd24b860806180247837fb6f2c2b
Karl Eklund <localpart@gmail.com>
2025-03-05 19:58:39 +0100

    customizations

--- custom.el
+++ custom.el
@@ -4,10 +4,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("02d422e5b99f54bd4516d4157060b874d14552fe613ea7047c4a5cfa1288cf4f"
+   '("d481904809c509641a1a1f1b1eb80b94c58c210145effc2631c1a7f2e4a2fdf4"
+     "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
+     "1f292969fc19ba45fbc6542ed54e58ab5ad3dbe41b70d8cb2d1f85c22d07e518"
+     "02d422e5b99f54bd4516d4157060b874d14552fe613ea7047c4a5cfa1288cf4f"
      "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a"
      default))
- '(make-backup-files nil))
+ '(indent-tabs-mode nil)
+ '(lsp-auto-execute-action nil)
+ '(make-backup-files nil)
+ '(scroll-margin 4)
+ '(typescript-ts-mode-indent-offset 4)
+ '(xref-show-xrefs-function 'consult-xref))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
